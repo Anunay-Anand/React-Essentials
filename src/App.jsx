f9677454b80b8d1cf1123d76528b294fb49a0cc5
Anunay Anand <anunayanand@github.com>
2025-06-04 00:20:15 +0530

    Adding Props to Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
-import reactImg from './assets/react-core-concepts.png';
+import reactImg from "./assets/react-core-concepts.png";
+import { CORE_CONCEPTS } from "./data.js";
 
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 
 function genRandomInt(max) {
   return Math.floor(Math.random() * (max + 1));
 }
 
+function CoreComponent(props) {
+  return (
+    <li>
+      <img src={props.image} title={props.title} />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  );
+}
+
 function Header() {
-  const description = reactDescriptions[genRandomInt(reactDescriptions.length - 1)];
+  const description =
+    reactDescriptions[genRandomInt(reactDescriptions.length - 1)];
   return (
     <header>
       <img src={reactImg} />
@@ -25,7 +37,31 @@ function App() {
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreComponent
+              title={CORE_CONCEPTS[0].title}
+              image={CORE_CONCEPTS[0].image}
+              description={CORE_CONCEPTS[0].description}
+            />
+            <CoreComponent
+              title={CORE_CONCEPTS[1].title}
+              image={CORE_CONCEPTS[1].image}
+              description={CORE_CONCEPTS[1].description}
+            />
+            <CoreComponent
+              title={CORE_CONCEPTS[2].title}
+              image={CORE_CONCEPTS[2].image}
+              description={CORE_CONCEPTS[2].description}
+            />
+            <CoreComponent
+              title={CORE_CONCEPTS[3].title}
+              image={CORE_CONCEPTS[3].image}
+              description={CORE_CONCEPTS[3].description}
+            />
+          </ul>
+        </section>
       </main>
     </div>
   );
