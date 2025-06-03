7c4c00c049fefae30c95671f9deb3744747986bb
Anunay Anand <anunayanand@github.com>
2025-06-04 00:50:20 +0530

    Adding Components in different files

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,6 @@
-import reactImg from "./assets/react-core-concepts.png";
 import { CORE_CONCEPTS } from "./data.js";
-
-const reactDescriptions = ["Fundamental", "Crucial", "Core"];
-
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function CoreComponent(props) {
-  return (
-    <li>
-      <img src={props.image} title={props.title} />
-      <h3>{props.title}</h3>
-      <p>{props.description}</p>
-    </li>
-  );
-}
-
-function Header() {
-  const description =
-    reactDescriptions[genRandomInt(reactDescriptions.length - 1)];
-  return (
-    <header>
-      <img src={reactImg} />
-      <h1>React Essentials</h1>
-      <p>
-        {description} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  );
-}
+import Header from "./components/Header.jsx";
+import CoreComponent from "./components/CoreConcept.jsx";
 
 function App() {
   return (
