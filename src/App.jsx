56497e0d741bd6141fc01056460839992e5ea606
Anunay Anand <anunayanand@github.com>
2025-06-06 01:00:18 +0530

    Adding state and tab contenent component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
-import { CORE_CONCEPTS } from "./data.js";
+import { useState } from 'react';
+
+import { CORE_CONCEPTS, EXAMPLES } from "./data.js";
 import Header from "./components/Header.jsx";
 import CoreComponent from "./components/CoreConcept.jsx";
+import TabButton from "./components/TabButton.jsx";
+import TabContent from './components/TabContent.jsx';
 
 function App() {
+
+  // Using our first useState
+  const [ selectedTopic, setSelectedTopic ] = useState();
+
+  function handleSelect(selectedButton) {
+    setSelectedTopic(selectedButton);
+  }
+
+  console.log('App Components loading');
   return (
     <div>
       <Header />
@@ -15,6 +28,17 @@ function App() {
             <CoreComponent {...CORE_CONCEPTS[2]} />
             <CoreComponent {...CORE_CONCEPTS[3]} />
           </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
+            <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
+          </menu>
+          {selectedTopic && <TabContent {...EXAMPLES[selectedTopic]} />}
+          {!selectedTopic && <div>Please click a Button</div> }
         </section>
       </main>
     </div>
